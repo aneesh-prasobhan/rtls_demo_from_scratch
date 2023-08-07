e09bf801b239f1a454b3e671d7b2dd5e07e8c1f3
Aneesh Prasobhan <aprasobhan@hard-softwerk.com>
2023-08-07 20:22:37 +0200

    Finished Basic Frame and Navbar

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import aoabutton from "./buttons/aoa_button.png";
+import rssibutton from "./buttons/rssi_fixed_hubs_button.png";
+import rtlslogo from "./rtls_logo.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+export {
+  rtlslogo,
+  aoabutton,
+  rssibutton,
+  menu,
+  close,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,27 @@
+import {
+    aoabutton,
+    rssibutton,
+
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Angle of Arrival Positioning",
+      icon: aoabutton,
+    },
+    {
+      title: "Fixed Hub RSSI Positioning",
+      icon: rssibutton,
+    },
+  ];
+
+
+  
+  export {services};
