385516c301f9f82a4266b61a7593843c2cf79112
aneeshprasobhan <aneeshprasobhan@gmail.com>
2023-08-11 00:24:30 +0200

    removed unnecessary files.

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,3 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
 import MapsCanvas from "./Maps";
-import StarsCanvas from "./Stars";
 
-export { EarthCanvas, BallCanvas, MapsCanvas, StarsCanvas };
+export { MapsCanvas };
